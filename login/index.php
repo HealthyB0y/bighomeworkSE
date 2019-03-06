f9f25feaef0092c5a3eb79e1e684423d9739d853
Hoàng <Huyhoang.ttw@gmail.com>
2019-03-06 14:16:46 +0700

    asdasd

---
+++ login/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>login</title>
+</head>
+<body style="text-align: center;">
+	<h2>Log in</h2>
+	<br>
+	<table top='40%' width='40%' border='0' style='font-family: Verdana; font-size: 14pt'>
+		<tr>
+			<td >Username :</td>
+			<td ><input type='text' name='username' size='25' value ='' style='font-family: Verdana; font-size: 14pt' /></td>
+		</tr>
+		<tr>
+			<td>Password :</td>
+			<td><input type='text' name='password' value ='' size='25' style='font-family: Verdana; font-size: 14pt' /></td>
+		</tr>
+	</table>
+</body>
+</html>
+
+<?php
+
+?>
